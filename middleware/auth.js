407b6fc721b4dd365e3babae1b029443e5435d83
Sujish Pradeep <sujishp164@gmail.com>
2019-09-26 15:21:20 +1000

    Disabled auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,46 +3,46 @@ const https = require("https");
 const { Account } = require("../models/account");
 
 module.exports = async function(req, res, next) {
-  let gtoken = req.header("x-auth-gtoken");
+  //   let gtoken = req.header("x-auth-gtoken");
 
-  if (gtoken) {
-    gtoken = gtoken.slice(1, -1);
+  //   if (gtoken) {
+  //     gtoken = gtoken.slice(1, -1);
 
-    console.log("gtoken", gtoken);
+  //     console.log("gtoken", gtoken);
 
-    const url = `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${gtoken}`;
-    console.log("url ", url);
+  //     const url = `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${gtoken}`;
+  //     console.log("url ", url);
 
-    await https
-      .get(url, resp => {
-        let data = "";
+  //     await https
+  //       .get(url, resp => {
+  //         let data = "";
 
-        // A chunk of data has been recieved.
-        resp.on("data", chunk => {
-          data += chunk;
-          console.log("data chunk ", data);
-        });
+  //         // A chunk of data has been recieved.
+  //         resp.on("data", chunk => {
+  //           data += chunk;
+  //           console.log("data chunk ", data);
+  //         });
 
-        // The whole response has been received. Print out the result.
-        resp.on("end", () => {
-          console.log("data end2", typeof data);
-          const JSON_data = JSON.parse(data);
-          console.log("data end3", JSON_data.email);
-          if (!JSON_data.email) {
-            console.log("access denied");
-            return res.status(401).send("Access denied. Invalid token.");
-          }
-        });
+  //         // The whole response has been received. Print out the result.
+  //         resp.on("end", () => {
+  //           console.log("data end2", typeof data);
+  //           const JSON_data = JSON.parse(data);
+  //           console.log("data end3", JSON_data.email);
+  //           if (!JSON_data.email) {
+  //             console.log("access denied");
+  //             return res.status(401).send("Access denied. Invalid token.");
+  //           }
+  //         });
 
-        next();
-      })
-      .on("error", err => {
-        console.log("access denied");
-        return res.status(401).send("Access denied. Invalid token.");
-      });
-  }
+  //         next();
+  //       })
+  //       .on("error", err => {
+  //         console.log("access denied");
+  //         return res.status(401).send("Access denied. Invalid token.");
+  //       });
+  //   }
 
-  // next();
+  next();
   // try {
   //   const decoded = jwt.verify(token, "vidly_jwtPrivateKey");
   //   req.user = decoded;
